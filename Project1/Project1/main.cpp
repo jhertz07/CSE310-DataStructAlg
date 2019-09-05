17abb93adafa0542470a4b53ab7c00c1282122d2
Justin Hertzberg <jhertzberg98@gmail.com>
2019-09-05 09:47:56 -0700

    started Xcode proj

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Justin Hertzberg on 9/5/19.
+//  Copyright © 2019 ASU. All rights reserved.
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Input string here\n"; // input string to be encoded
+    string word;
+    cin >> word; // store in 'word'
+    
+    
+    
+    return 0;
+}
