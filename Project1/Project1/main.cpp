e4f16ce1f00a4db9efa56f082475f812ce563b2d
Justin Hertzberg <jhertzberg98@gmail.com>
2019-09-05 15:20:37 -0700

    began shifting original word and saving to array of arrays (pointers)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -15,7 +15,23 @@ int main(int argc, const char * argv[]) {
     string word;
     cin >> word; // store in 'word'
     
+    //put string into an array
+    int length = (unsigned)word.length(); // get length of string
+    char* word_arr = new char[length]; // create char array using length
+    strcpy(word_arr, word.c_str()); // copies string to char array
     
+        // print char array for testing
+        for (int i = 0; i < length; i++) {
+            cout << word_arr[i] << " ";
+        }
+    
+    // create an array of pointers to store each array of the shifted word
+    char** original_arr = new char* [length+1];
+    
+    // move each char over to the left and save into new array
+    for (int i = 0; i <= length+1; i++) {
+        original_arr[i] = word_arr;
+    }
     
     return 0;
 }
