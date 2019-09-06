b720430365ee577d9e5df944be866d2d8122f395
Justin Hertzberg <jhertzberg98@gmail.com>
2019-09-06 09:57:36 -0700

    inputted word successfully shifted to the left the correct number of time

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -20,17 +20,26 @@ int main(int argc, const char * argv[]) {
     char* word_arr = new char[length]; // create char array using length
     strcpy(word_arr, word.c_str()); // copies string to char array
     
-        // print char array for testing
-        for (int i = 0; i < length; i++) {
-            cout << word_arr[i] << " ";
-        }
-    
     // create an array of pointers to store each array of the shifted word
     char** original_arr = new char* [length+1];
+    char temp; // used for shifting array elements
     
     // move each char over to the left and save into new array
-    for (int i = 0; i <= length+1; i++) {
+    for (int i = 0; i < length; i++) {
+        
+        // print char array for testing
+        for (int v = 0; v < length; v++) {
+            cout << word_arr[v] << " ";
+        }
+        cout << "\n";
+        
         original_arr[i] = word_arr;
+        temp = word_arr[0];
+        // loop to shift elements left
+        for (int j = 0; j < length-1; j++) { // stop loop one before last element
+            word_arr[j] = word_arr[j+1];
+        }
+        word_arr[length-1] = temp; // make last element equal temp (first element)
     }
     
     return 0;
