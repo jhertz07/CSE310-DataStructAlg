7a1de6c259a4a0f16794158cea482382f27e076b
Justin Hertzberg <jhertzberg98@gmail.com>
2019-09-12 16:23:59 -0700

    changed array names

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -17,11 +17,11 @@ int main(int argc, const char * argv[]) {
     
     //put string into an array
     int length = (unsigned)word.length(); // get length of string
-    char* word_arr = new char[length]; // create char array using length
-    strcpy(word_arr, word.c_str()); // copies string to char array
+    char* input_arr = new char[length]; // create char array using length
+    strcpy(input_arr, word.c_str()); // copies string to char array
     
     // create an array of pointers to store each array of the shifted word
-    char** original_arr = new char* [length-1];
+    char** words_arr = new char* [length-1];
     char temp; // used for shifting array elements
     
     // move each char over to the left and save into new array
@@ -29,25 +29,25 @@ int main(int argc, const char * argv[]) {
         
 //        // print char array for testing
 //        for (int v = 0; v < length; v++) {
-//            cout << word_arr[v] << " ";
+//            cout << input_arr[v] << " ";
 //        }
 //        cout << "\n";
         
-        original_arr[i] = word_arr;
-        temp = word_arr[0];
+        words_arr[i] = input_arr;
+        temp = input_arr[0];
         // loop to shift elements left
         for (int j = 0; j < length-1; j++) { // stop loop one before last element
-            word_arr[j] = word_arr[j+1];
+            input_arr[j] = input_arr[j+1];
         }
-        word_arr[length-1] = temp; // make last element equal temp (first element)
-        cout << original_arr[i] <<"\n";
+        input_arr[length-1] = temp; // make last element equal temp (first element)
+        cout << words_arr[i] <<"\n";
     }
     
     
 //    for (int i = 0; i <= length; i++) {
 //        cout << i << "\n";
 //        for (int j = 0; j < length; j++) {
-//            cout << original_arr[i] << " ";
+//            cout << words_arr[i] << " ";
 //        }
 //        cout << "\n";
 //    }
