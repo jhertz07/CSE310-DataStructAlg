a625a9ebbaf8fdccb41d5e6b91103883c7dc3e7b
Justin Hertzberg <jhertzberg98@gmail.com>
2019-09-28 14:13:50 -0700

    before rewriting with asu.general version

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -8,15 +8,18 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 using namespace std;
 
 char** insertionSort(char**, int);
 void print_arr(char**, int);
+char** encode (char**, int, string);
 
 int main(int argc, const char * argv[]) {
-    cout << "Input string here\n"; // input string to be encoded
+    //cout << "Input string here\n"; // input string to be encoded
+    if (strcmp(argv[0], "insertion")) {
     string word;
-    cin >> word; // store in 'word'
+    while(!getline(cin, word).eof()) { // store input in 'word'
     
     //put string into an array
     int length = (unsigned)word.length(); // get length of string
@@ -44,11 +47,20 @@ int main(int argc, const char * argv[]) {
         }
         shift_arr[i][length-1] = temp; // make last element equal temp (first element)
     }
-    cout << "\nprinted array:\n";
-    print_arr(shift_arr, length);
+    //cout << "\nprinted array:\n";
+    //print_arr(shift_arr, length);
     insertionSort(shift_arr, length);
-    cout << "\nsorted array:\n";
-    print_arr(shift_arr, length);
+    //cout << "\nsorted array:\n";
+    //print_arr(shift_arr, length);
+    encode(shift_arr, length, word);
+    
+    // delete dynamic array
+        for (int i = 0; i < length; ++i) {
+            delete[] shift_arr[i];
+        }
+        delete[] shift_arr;
+    }
+        
     return 0;
 }
 
@@ -59,7 +71,20 @@ char** insertionSort(char** unsorted_arr, int length) {
     for (int i = 1; i < length; i++) {
         j = i;
         // find ASCII value by printing int(char)
-        while (j > 0 && int(sorted[j-1][length-1]) > int(sorted[j][length-1])) {
+        while (j > 0 && int(sorted[j-1][0]) >= int(sorted[j][0])) {
+            // if ascii value equal, then check next letter until a non-equivalence is found
+            if (int(sorted[j-1][0]) == int(sorted[j][0])) {
+                int z = 1;
+                while (int(sorted[j-1][z]) == int(sorted[j][z])) {
+                    if ((z+1) == length) break;
+                    z++;
+                }
+                // if the letter is less than or equal in ascii value, then stop shifting
+                if (int(sorted[j-1][z]) <= int(sorted[j][z])) {
+                    break;
+                }
+            }
+            // shift index to the left
             temp = sorted[j];
             strcpy(sorted[j], sorted[j-1]);
             strcpy(sorted[j-1], temp.c_str());
@@ -68,7 +93,34 @@ char** insertionSort(char** unsorted_arr, int length) {
     }
     return sorted;
 }
+char** encode (char** array, int length, string original) {
+    char** this_array = array; // copy array to local variable
+    char output; // char to be outputted (last letter of each string
+    int cluster = 1; // size of cluster
+    // find and output original index
+    for (int i = 0; i < length; i++) {
+        // check for original word index
+        if (this_array[i] == original) {
+            // add index number to first index of encoded array
+            cout << i << "\n";
+        }
+    }
+    // encoded output
+    for (int i = 0; i < length; i++) {
+        output = this_array[i][length-1];
+        if ((i < (length-1)) && this_array[i+1][length-1] == output) {
+            cluster++;
+        }
+        else {
+            cout << cluster << output;
+            cluster = 1;
+        }
+    }
+    cout << "\n";
+    return this_array;
+}
 
+// print function
 void print_arr(char** arr, int length) {
     for (int i = 0; i < length; i++) {
         cout << arr[i] << "\n";
