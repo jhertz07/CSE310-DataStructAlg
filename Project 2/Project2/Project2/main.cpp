f0d6bd58a4816193a2a7e00b7e9f9f3e8f42c8f5
Justin <jhertzberg98@gmail.com>
2019-10-17 17:51:49 -0700

    can read all files from input

--- Project 2/Project2/Project2/main.cpp
+++ Project 2/Project2/Project2/main.cpp
@@ -12,29 +12,57 @@
 #include <fstream>
 #include <string>
 
+void initialize_storm(storm_event);
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     // open csv files
     ifstream details_1950;
     details_1950.open("details-1950.csv");
-    if (details_1950.is_open()) {
-        cout << "worked\n";
-    }
+    ifstream fatalities_1950;
+    fatalities_1950.open("fatalities-1950.csv");
+    ifstream details_1951;
+    details_1951.open("details-1951.csv");
+    ifstream fatalities_1951;
+    fatalities_1951.open("fatalities-1951.csv");
+    ifstream details_1952;
+    details_1952.open("details-1952.csv");
+    ifstream fatalities_1952;
+    fatalities_1952.open("fatalities-1952.csv");
     
     int num_of_years;
     // get num of years thru command line arg
     if ((num_of_years = atoi(argv[1])));
     else return 0;
     // initialize dynamic array to store years from command line args
-    int *years = new int[num_of_years];
+    annual_storms *year = new annual_storms[num_of_years];
+    string line; int num_of_lines = 0; // used for getline in loop
+    
+    // initialize each year of storms
     for (int i = 0; i < num_of_years; i++) {
-        years[i] = atoi(argv[i+2]);
+        year[i].year = atoi(argv[i+2]); // match year to argument input
+        // find which file to read from
+        num_of_lines = 0;
+        if (year[i].year == 1950) {
+            while(getline(details_1950, line)) {
+                num_of_lines++;
+            }
+        }
+        else if (year[i].year == 1951) {
+            while(getline(details_1951, line)) {
+                num_of_lines++;
+            }
+        }
+        else if (year[i].year == 1952) {
+            while(getline(details_1952, line)) {
+                num_of_lines++;
+            }
+        }
     }
-    string temp; // store the csv line, needs updating
-    int detail_count = 0;
-    while (getline(details_1950, temp)) {
-        detail_count++;
-    }
-    cout << detail_count << "\n";
+    cout << num_of_lines << "\n";
+}
+
+void initialize_storm(storm_event storm) {
+    
 }
